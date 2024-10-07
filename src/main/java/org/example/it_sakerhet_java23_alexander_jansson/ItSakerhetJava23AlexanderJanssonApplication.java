0d43e40f2b1279526b44d4dab3b81e9888f49fb6
Alexander Jansson <alexanderjson@hotmail.com>
2024-10-07 17:44:20 +0200

    created folders

--- src/main/java/org/example/it_sakerhet_java23_alexander_jansson/ItSakerhetJava23AlexanderJanssonApplication.java
+++ src/main/java/org/example/it_sakerhet_java23_alexander_jansson/ItSakerhetJava23AlexanderJanssonApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ItSakerhetJava23AlexanderJanssonApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ItSakerhetJava23AlexanderJanssonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ItSakerhetJava23AlexanderJanssonApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/it_sakerhet_java23_alexander_jansson/ItSakerhetJava23AlexanderJanssonApplicationTests.java
+++ src/test/java/org/example/it_sakerhet_java23_alexander_jansson/ItSakerhetJava23AlexanderJanssonApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ItSakerhetJava23AlexanderJanssonApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
