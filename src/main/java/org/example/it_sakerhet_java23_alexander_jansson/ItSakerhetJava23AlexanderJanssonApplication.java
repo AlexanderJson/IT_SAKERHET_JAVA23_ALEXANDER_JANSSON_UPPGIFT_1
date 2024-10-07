8dfc2c0ec0bfdfc8621b9e24a0a4681843a09ecf
Alexander Jansson <alexanderjson@hotmail.com>
2024-10-07 17:33:17 +0200

    Initial commit

---
+++ src/main/java/org/example/it_sakerhet_java23_alexander_jansson/ItSakerhetJava23AlexanderJanssonApplication.java
@@ -0,0 +1,13 @@
+package org.example.it_sakerhet_java23_alexander_jansson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSakerhetJava23AlexanderJanssonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSakerhetJava23AlexanderJanssonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/it_sakerhet_java23_alexander_jansson/ItSakerhetJava23AlexanderJanssonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.it_sakerhet_java23_alexander_jansson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSakerhetJava23AlexanderJanssonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
